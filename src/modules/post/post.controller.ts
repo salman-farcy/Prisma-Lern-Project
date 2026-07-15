e32efe6c8c09edaee62af613fc0575db0b7887b0
slaman-farcy <salmanfarcy253@gmail.com>
2026-07-15 12:32:08 +0600

    createPost route created

--- src/modules/post/post.controller.ts
+++ src/modules/post/post.controller.ts
@@ -1,16 +1,28 @@
+import  httpStatus  from 'http-status';
 import { NextFunction, Request, Response } from "express";
 import { catchAsync } from "../../utils/catchAsync";
+import { postServices } from "./post.services";
+import { sendResponse } from "../../utils/sendResponse";
 
 
 
 const createPost = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+     const id = req.user?.id;
+     const payload = req.body;
 
+     const postResult = await postServices.createPostDB(payload, id as string)
 
+     sendResponse(res, {
+          success: true,
+          statusCode: httpStatus.CREATED,
+          message: "Post Created Successfully",
+          data: postResult 
+     })
 });
 
 
 const getAllPosts = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
-
+     
 
 });
 
--- src/modules/post/post.services.ts
+++ src/modules/post/post.services.ts
@@ -1,8 +1,16 @@
+import { prisma } from "../../lib/prisma"
+import { ICreatePostPayload } from "./post.interface"
 
 
 
-const createPostDB = async (payload: ICreatePostPayload) => {
-
+const createPostDB = async (payload: ICreatePostPayload, userId: string) => {
+      const result = await  prisma.post.create({
+          data: {
+               ...payload,
+               authorId: userId
+          }
+      });
+      return result;
 }
 
 
